7c41eeda57e2bcd60b129a9071f32b4dd371cf87
Nithin <rnithin@berkeley.edu>
2021-05-05 16:30:32 -0400

    added scene transitioning

--- Assets/TeleporterOutside.cs
+++ Assets/TeleporterOutside.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Teleporter : MonoBehaviour
+public class TeleporterOutside : MonoBehaviour
 {
     public Canvas canvas;
     private Collider coll;
-    private float timeToWait = 0.5f;
-    private float done = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,17 +29,13 @@ public class Teleporter : MonoBehaviour
 
     IEnumerator TeleportAndFadeIn(Collider other)
     {
-        StartCoroutine(canvas.GetComponent<UIController>().FadeBlackOutSquare(true));
+        //StartCoroutine(canvas.GetComponent<UIController>().FadeBlackOutSquare(true));
         yield return new WaitForSeconds(2);
-        CharacterController charController = other.GetComponent<CharacterController>();
-        charController.enabled = false;
-        Vector3 closestPoint = coll.ClosestPointOnBounds(other.transform.position);
+
         LookAtWallNormal(other);
-        float distance = Vector3.Distance(closestPoint, other.transform.position);
-        Vector3 new_pos = other.transform.position + 2 * distance * other.transform.forward;
-        charController.transform.position = new Vector3(new_pos.x, other.transform.position.y, new_pos.z);
-        charController.enabled = true;
-        StartCoroutine(canvas.GetComponent<UIController>().FadeBlackOutSquare(false));
+        SceneManager.LoadScene(1);
+
+        //StartCoroutine(canvas.GetComponent<UIController>().FadeBlackOutSquare(false));
     }
 
     void LookAtWallNormal(Collider other)
